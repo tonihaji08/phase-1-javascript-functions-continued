25bc2a6d31f2a4274a99d68114c179072055dc10
Your Name <antony.adriano@stundent.moringaschool.com>
2022-11-27 00:10:32 +0300

    Completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+console.log(saturdayFun("bathe my dog"));
+function mondayWork(work = "go to the office"){
+    return `This Monday, I will ${work}.`
+}
+console.log(mondayWork("work from home"));
+function wrapAdjective(visual = "*"){
+    return function(adjective = "special"){
+        return `You are ${visual}${adjective}${visual}!`;
+    };
+}
+console.log(wrapAdjective("||")("a dedicated programmer"));
